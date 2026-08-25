3aa2823fe02c6ad84cf03c7f845e0bc09cf797d0
auhsuai <auhsuai@gmail.com>
2026-08-25 15:59:02 +0700

    fix(test): ensure compose runtime on unit test classpath

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.8")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.compose.runtime:runtime")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
